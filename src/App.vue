90ce3839f23c0c6de423287ce4eb000f3ffbef1b
Angel Odiel Trevino Villanueva <angel.trevinov@gmail.com>
2020-12-25 17:11:06 -0600

    adding constellations and getting user position

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,114 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+
+      <div class="row">
+        <div class="col">
+          <div class="text-center h1 mt-4">
+            Welcome to Sky Explorer!
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col">
+          <p class="text-center">
+            To find out how the constellations appear above your or for a specific location, let the app use your location or set a location by submitting a location in the corresponding text field
+          </p>
+        </div>
+      </div>
+
+      <div class="form-group">
+        <div class="row input-group">
+          <div class="col-10">
+            <input type="text" class="form-control">
+          </div>
+          <div class="col-2">
+            <button type="button" class="btn btn-outline-light">Show Sky!</button>
+          </div>
+        </div>
+        <div class="row mt-3">
+          <div class="col-12  text-center ">
+            <button type="button" class="btn btn-outline-info" @click="getUserLocation">Use My Location</button>
+          </div>
+        </div>
+      </div>
+
+      <div class="row mt-3" style="width: 100%; height: 60%;">
+        <div class="col">
+          <VirtualSky v-bind:location="location"/>
+        </div>
+      </div>
+
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import VirtualSky from './components/VirtualSky.vue'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      location: null
+    }
+  },
   components: {
-    HelloWorld
+    VirtualSky
+  },
+  methods: {
+    // -----------------------------------------------------
+    getUserLocation() {
+
+      if (!('geolocation' in navigator)) {
+        alert("Please enable geolocation for this page");
+      } else {
+        navigator.geolocation.getCurrentPosition(
+            pos => {
+              this.location = pos;
+            }
+        );
+      }
+
+    }
   }
 }
 </script>
 
 <style>
+
+html,
+body,
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  background-color: #121212;
+  color: white;
+}
+
+.container {
+  height: 100%;
+}
+
+/* You can add global styles to this file, and also import other style files */
+.form-control {
+  background-color: #121212;
+  color: white;
+}
+
+.form-control:focus {
+  background-color: #121212;
+  color: white;
+}
+
+.form-control::placeholder {
+  color: #121212;
+}
+
+.input-group-text {
+  background-color: #121212;
+  color: white;
 }
 </style>
---
+++ src/components/VirtualSky.vue
@@ -0,0 +1,50 @@
+<template>
+  <iframe
+      frameborder="0"
+      scrolling="no"
+      marginheight="0"
+      marginwidth="0"
+      v-bind:src="linkApi"
+      allowTransparency="true">
+  </iframe>
+</template>
+
+<script>
+export default {
+  name: 'VirtualSky',
+  data() {
+    return {
+      linkApi: ''
+    }
+},
+  props: {
+    location: null
+  },
+  watch: {
+    location: function () {
+      if (this.location) {
+        this.linkApi = "https://virtualsky.lco.global/embed/index.html?" +
+            "longitude=" +
+            "&latitude=" + "34.4326" +
+            "&projection=" + "stereo" +
+            "&constellations=" + "true" +
+            "&constellationlabels=" + "true" +
+            "&meteorshowers=" + "true" +
+            "&showstarlabels=" + "true" +
+            "&showdate=false" +
+            "&az=162.03638296776467";
+      }
+      console.log(this.location);
+    }
+  }
+}
+</script>
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+iframe {
+  width: 100%;
+  height: 100%;
+}
+</style>
