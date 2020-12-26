3a31d976e4da0d645edd5be93916cdfcfb0302a2
Angel Odiel Trevino Villanueva <angel.trevinov@gmail.com>
2020-12-26 00:35:36 -0600

    adding src

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+/**
+ * Based on https://virtualsky.lco.global/embed/custom.html
+ */
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
